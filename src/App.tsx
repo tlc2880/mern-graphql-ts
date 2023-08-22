97256fcd4363681db2442d6ed6a683eb69aec6cd
tlc2880 <tommylcao@gmail.com>
2023-08-22 05:46:07 -0700

    add react client delete feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import Header from "./components/Header";
 import Clients from "./components/Clients";
-
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// This code is used to remove a warning from the console
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
--- src/components/Client.types.ts
+++ src/components/Client.types.ts
@@ -1,9 +1,14 @@
 export type Client = {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-  };
-  export type ClientProps = {
-    client: Client;
-  };
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+};
+
+export type ClientProps = {
+  client: Client;
+};
+
+export type ClientType = {
+  client: Client;
+};
