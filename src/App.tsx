7dd1557ca8b77985ed6f5041dca3968465ed3944
tlc2880 <tommylcao@gmail.com>
2023-08-25 05:48:00 -0700

    Add react project info router page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Header from "./components/Header";
-import Clients from "./components/Clients";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import AddClientModal from './components/AddClientModal';
-import Projects from "./components/Projects";
+import Header from "./components/Header";
+import Home from './pages/Home';
+import Project from './pages/Project';
+import NotFound from './pages/NotFound';
 
 // This code is used to remove a warning from the console
 const cache = new InMemoryCache({
@@ -33,12 +34,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+      <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
